7d13b259613dada05c1a55a8efa691ab2c184940
chanwit-y <chanwit.yimneam@hotmail.com>
2023-04-01 14:13:40 +0700

    set global get user profile by user id

--- engine/internal/v8/api.go
+++ engine/internal/v8/api.go
@@ -1,16 +1,39 @@
 package v8
 
 import (
+	"encoding/json"
+	"engine/internal/api"
 	"fmt"
 
 	v8 "rogchap.com/v8go"
 )
 
-func GetUserProfileByUserId(iso *v8.Isolate, global *v8.ObjectTemplate) {
+type (
+	ApiFuncTemplate interface {
+		SetupGetUserProfileByUserId(iso *v8.Isolate, global *v8.ObjectTemplate)
+	}
+	apiFuncTemplate struct {
+		userProfileApi api.UserProfileApi
+	}
+)
+
+func NewApiFuncTemplate(userProfileApi api.UserProfileApi) ApiFuncTemplate {
+	return apiFuncTemplate{userProfileApi}
+}
+
+func (a apiFuncTemplate) SetupGetUserProfileByUserId(iso *v8.Isolate, global *v8.ObjectTemplate) {
 	getUserProfileByUserId := v8.NewFunctionTemplate(iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
 		userId := info.Args()[0]
-		fmt.Printf("userId: %v \n", userId) // when the JS function is called this Go callback will execute
-		return nil                          // you can return a value back to the JS caller if required
+
+		res, _ := a.userProfileApi.GetByUserId(userId.String())
+
+		jsonBytes, _ := json.Marshal(res)
+
+		ctx := v8.NewContext(iso, global)
+		val, _ := v8.JSONParse(ctx, string(jsonBytes))
+
+		fmt.Printf("val: %+v \n", val)
+		return val
 	})
-	global.Set("getUserProfileByUserId", getUserProfileByUserId) // sets the "print" property of the Object to our function
+	global.Set("getUserProfileByUserId", getUserProfileByUserId)
 }
--- engine/main.go
+++ engine/main.go
@@ -1,18 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"engine/internal/api"
 	http "engine/pkg/http"
 	"fmt"
+
+	aft "engine/internal/v8"
+
+	v8 "rogchap.com/v8go"
 )
 
 func main() {
 	acquireToken, _ := http.NewBanpuAcquireToken(false)
 	httpClient := http.NewHttpClient(acquireToken)
-
 	userProfileApi := api.NewUserProfileApi(httpClient)
 
-	res, _ := userProfileApi.GetByUserId("dev-52")
+	iso := v8.NewIsolate()              // create a new VM
+	global := v8.NewObjectTemplate(iso) // a template that represents a JS Object
+	cpuProfiler := v8.NewCPUProfiler(iso)
 
-	fmt.Printf("user profile: %+v", res)
+	apiFuncTemplate := aft.NewApiFuncTemplate(userProfileApi)
+	// must set global function before create context
+	apiFuncTemplate.SetupGetUserProfileByUserId(iso, global)
+
+	ctx := v8.NewContext(iso, global)
+
+	cpuProfiler.StartProfiling("profile-1")
+
+	ctx.RunScript(`
+		const userProfile = getUserProfileByUserId('dev-52');
+	`, "main.js") // any functions previously added to the context can be called
+
+	v8UserProfile, _ := ctx.RunScript("userProfile", "value.js") // return a value in JavaScript back to Go
+	x, _ := json.Marshal(v8UserProfile)
+
+	fmt.Printf("userProfile: %+v \n", string(x))
+
+	cpuProfile := cpuProfiler.StopProfiling("profile-1")
+	printTree("", cpuProfile.GetTopDownRoot())
+}
+
+func printTree(nest string, node *v8.CPUProfileNode) {
+	fmt.Printf("%s%s %s:%d:%d\n", nest, node.GetFunctionName(), node.GetScriptResourceName(), node.GetLineNumber(), node.GetColumnNumber())
+	count := node.GetChildrenCount()
+	if count == 0 {
+		return
+	}
+	nest = fmt.Sprintf("%s  ", nest)
+	for i := 0; i < count; i++ {
+		printTree(nest, node.GetChild(i))
+	}
 }
