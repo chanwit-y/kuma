9039b2cf9cb2ca4f93e9aa9565dd60ea2f098244
chanwit-y <chanwit.yimneam@hotmail.com>
2023-04-30 22:44:50 +0700

    rust fn

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -3,8 +3,24 @@
   windows_subsystem = "windows"
 )]
 
+use std::process::Command;
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![create_project])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+   format!("Hello, {}!", name)
+}
+
+#[tauri::command]
+fn create_project(name: &str)  {
+  let mut list_dir = Command::new("ls");
+ 
+  //  format!("create project, {}!", name)
 }
