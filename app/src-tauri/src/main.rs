0c4f16a5d3612bbf7a53e558a063c436197f4819
chanwit-y <chanwit.yimneam@hotmail.com>
2023-05-01 11:11:40 +0700

    call rust create project

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,26 +1,36 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 use std::process::Command;
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .invoke_handler(tauri::generate_handler![create_project])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![go_mod_init])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+    format!("Hello, {}!", name)
 }
 
 #[tauri::command]
-fn create_project(name: &str)  {
-  let mut list_dir = Command::new("ls");
- 
-  //  format!("create project, {}!", name)
+fn go_mod_init() {
+    let go_mod_result = Command::new("go")
+        .arg("mod")
+        .arg("init")
+        .arg("output")
+        .current_dir("/Users/chanwityimneam/Desktop/Project/kuma/out")
+        .status()
+        .expect("failed to execute go mod init command");
+
+    if go_mod_result.success() {
+        println!("Initialized go module successfully");
+    } else {
+        println!("Failed to initialize go module");
+    }
 }
