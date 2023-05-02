8aabe20a0cc96af64d7bb3a959ffc739fd45cb2e
chanwit-y <chanwit.yimneam@hotmail.com>
2023-05-02 23:19:09 +0700

    project

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,30 +1,11 @@
-import { invoke } from '@tauri-apps/api/tauri'
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { useEffect } from 'react'
-import Link from 'next/link';
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  useEffect(() => {
-    invoke('greet', { name: 'World' })
-      .then(console.log)
-      .catch(console.error)
-  }, []);
 
   return (
-    <>
-      <Link href='/project'>
-        project
-      </Link>
-      <button onClick={() => {
-        invoke('go_mod_init')
-          .then(console.log)
-          .catch(console.error)
-      }}>create project</button>
-    </>
+    <Link href="project">
+      project
+    </Link>
   )
 }
--- app/src/pages/project/index.tsx
+++ app/src/pages/project/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { useMemo } from 'react'
+import { Box, Card, Grid, Typography } from "@mui/material";
+import { Project } from '@/@types';
 
 export default function Project() {
-  return (
-    <div>Project</div>
-  )
+  const projects = useMemo<Project[]>(() => [
+    { name: "DOA", description: "-" },
+    { name: "TAP", description: "-" },
+  ], []);
+
+  return (<Grid container spacing={2}>
+    {projects.map((p) => <Grid item md={4} sm={4}>
+      <Card>
+        <Box p={2}  >
+          <Typography>
+            {p.name}
+          </Typography>
+          <Typography>
+            {p.description}
+          </Typography>
+        </Box>
+      </Card>
+    </Grid>)}
+  </Grid>)
 }
