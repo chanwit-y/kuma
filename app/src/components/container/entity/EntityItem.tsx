3b26e9e95e559b5c0ad8f076e48fb27d5dc26ac9
chanwit-y <chanwit.yimneam@hotmail.com>
2023-05-24 21:11:03 +0700

    action

--- app/src/components/container/entity/EntityItem.tsx
+++ app/src/components/container/entity/EntityItem.tsx
@@ -1,7 +1,10 @@
 import { Box, Typography } from '@mui/material';
-import { blue, red } from '@mui/material/colors';
+import { blue, grey, red } from '@mui/material/colors';
 import React, { memo, useEffect } from 'react'
 import { Handle, Position } from 'reactflow';
+
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 
 type Props = {
 	// type: HandleType;
@@ -15,17 +18,30 @@ export const EntityItem = memo(({ handleId, column }: Props) => {
 	}, [column]);
 	return (
 		<Box position='relative'>
-			<Box display="flex" py={.5} pl={1} >
-				{column.isPK ? <Box p={.1} px={.2} mr={.5} borderRadius={.2} sx={{ backgroundColor: blue[400] }} >
-					<Typography fontSize={6} color="white">PK</Typography>
-				</Box> : null}
-				<Typography fontSize={8}>
-					{`${column.name}:`}
-				</Typography>
-				<Typography mx={.7} fontSize={8} letterSpacing={.4}>
-					{`[${column.dataType}]`}
-				</Typography>
+			<Box
+				display="flex"
+				justifyContent="space-between"
+				alignItems="center"
+				p={.5} >
+				<Box display="flex"  >
+					<DragIndicatorIcon  sx={{fontSize: 10, color: grey[400]}} />
+					{column.isPK ? <Box p={.1} px={.2} mr={.5} borderRadius={.2} sx={{ backgroundColor: blue[400] }} >
+						<Typography fontSize={6} color="white">PK</Typography>
+					</Box> : null}
+					<Typography fontSize={8}>
+						{`${column.name}:`}
+					</Typography>
+					<Typography mx={.5} fontSize={8} letterSpacing={.4}>
+						{`${column.dataType}`}
+					</Typography>
+					{column.length && (<Typography fontSize={8} letterSpacing={.4}>
+						{`(${column.length})`}
+					</Typography>)}
 
+				</Box>
+				<Box>
+					<MoreVertIcon sx={{fontSize: 7}} />
+				</Box>
 			</Box>
 			{
 				(column.isPK || column.isFK) ? (
--- app/src/components/container/entity/EntityNode.tsx
+++ app/src/components/container/entity/EntityNode.tsx
@@ -1,7 +1,10 @@
 import React, { memo, useEffect } from 'react'
 import { EntityItem } from './EntityItem'
 import { Box, Divider, Typography } from '@mui/material'
-import { blue } from '@mui/material/colors'
+import { blue, grey } from '@mui/material/colors'
+
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
 
 type Props = {
   id?: string,
@@ -17,13 +20,23 @@ export const EntityNode = memo(({ id, data }: Props) => {
   return (
     // <Box p={1} width={120} height="auto" bgcolor="white">
     <Box >
-      <Box bgcolor={blue[100]} sx={{ borderTopLeftRadius: 2, borderTopRightRadius: 2, }}>
-        <Typography variant='subtitle1' letterSpacing={.5} p={.5} fontSize={8}>{data.table.name}</Typography>
+      <Box
+        display='flex'
+        justifyContent='space-between'
+        alignItems='center'
+        bgcolor={blue[100]}
+        sx={{ borderTopLeftRadius: 2, borderTopRightRadius: 2, }}>
+        <Typography variant='subtitle1' letterSpacing={.5} p={.5} fontSize={8} fontWeight='bold'>{data.table.name}</Typography>
+        <Box display='flex' mr={.5} gap={.5}>
+          <BorderColorIcon sx={{fontSize: 10, color: grey[700]}} />
+          <AddCircleIcon sx={{fontSize: 10, color: grey[700]}} />
+        </Box>
       </Box>
       <Divider />
       <Box >
         {data.table.columns.map((c: any) => <>
-          <EntityItem  handleId={c.name} column={c} />
+          <EntityItem handleId={c.name} column={c} />
+
           {/* <Divider  /> */}
         </>)}
         {/* {JSON.stringify(data.table, undefined, 2)}
