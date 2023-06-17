7fa066c3f089eb8019c0d714d9ead3d41e5966c3
chanwit-y <chanwit.yimneam@hotmail.com>
2023-06-17 15:21:56 +0700

    column name tooltip

--- app/src/components/container/entity/EntityItem.tsx
+++ app/src/components/container/entity/EntityItem.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, IconButton, Popover, Typography } from '@mui/material';
+import { Box, Button, Divider, IconButton, Popover, Tooltip, Typography } from '@mui/material';
 import { blue, grey, red } from '@mui/material/colors';
 import React, { memo, useState, MouseEvent } from 'react'
 import { Handle, Position } from 'reactflow';
@@ -37,18 +37,27 @@ export const EntityItem = memo(({ handleId, column }: Props) => {
 			bgcolor={isHighlight ? blue[50] : undefined}
 			sx={{ transition: "background-color 0.5s ease" }}>
 			<Box
+				width="100%"
 				display="flex"
 				justifyContent="space-between"
 				alignItems="center"
-				p={.5} py={.3} >
+				px={.5} py={.3} >
 				<Box display="flex" alignItems='center'  >
 					<DragIndicatorIcon sx={{ fontSize: 10, color: grey[400] }} />
 					{column.isPK ? <Box p={.1} px={.2} mr={.5} borderRadius={.2} sx={{ backgroundColor: blue[400] }} >
 						<Typography fontSize={6} color="white" letterSpacing={.1}>PK</Typography>
 					</Box> : null}
-					<Typography fontSize={8} letterSpacing={.1}>
-						{`${column.name}`}
-					</Typography>
+					<Box width={column.isPK ? 20 : 35} maxWidth={35}>
+						<Tooltip title={column.name}>
+							<Typography
+								overflow={'hidden'}
+								textOverflow='ellipsis'
+								fontSize={8}
+								letterSpacing={.1}>
+								{`${column.name}`}
+							</Typography>
+						</Tooltip>
+					</Box>
 					{/* <Divider orientation='vertical' sx={{color: red}} /> */}
 					<Typography mx={.5} fontSize={8} color={blue[900]} fontWeight='bold' letterSpacing={.4}>
 						{`${column.dataType}`}
@@ -86,7 +95,7 @@ export const EntityItem = memo(({ handleId, column }: Props) => {
 							horizontal: 'left',
 						}}
 					>
-						<ColumnInfo onAddColumn={() =>{}} />
+						<ColumnInfo onAddColumn={() => { }} />
 					</Popover>
 				</Box>
 			</Box>
@@ -99,6 +108,6 @@ export const EntityItem = memo(({ handleId, column }: Props) => {
 					/>
 				) : null
 			}
-		</Box>
+		</Box >
 	)
 })
