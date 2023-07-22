35fc6b4ac009e28d4a45fe7de06dbabae309773f
cy <chanwit.yimneam@hotmail.com>
2023-07-22 20:42:39 +0700

    add node exec

--- app/src/pages/project/index.tsx
+++ app/src/pages/project/index.tsx
@@ -1,16 +1,40 @@
 import { ProjectContainer } from "@/components/container/project";
 import { GetServerSideProps, NextPage } from "next";
 
-type Props = {}
 
-const Project: NextPage<Props> = ({ }: Props) => {
-  return (<ProjectContainer />)
+import { exec as execCb } from 'child_process';
+import { promisify } from 'util';
+import { Fragment } from "react";
+
+const exec = promisify(execCb);
+
+type Props = {
+  stdout: string;
+}
+
+const Project: NextPage<Props> = ({ stdout }: Props) => {
+  return (<Fragment>
+    {stdout}
+    <ProjectContainer />
+  </Fragment>)
 }
 
 export default Project;
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  let resutl: string = "";
+  try {
+    const { stdout, stderr } = await exec('ls -la');
+    resutl = stdout;
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+
   return {
-    props: {}
+    props: {
+      stdout: resutl,
+    }
   }
 }
